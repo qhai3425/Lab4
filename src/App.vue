859bb15d8ad82b01b6efb287c628933a4053b4d0
qhai3425 <haibt3k4@gmail.com>
2024-11-18 09:03:20 +0700

    add lab4

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<!-- <script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<script setup>
+import Hello from './components/Hello.vue';
+</script>
+
+<template>
+  <Hello />
+</template>
+
+<template>
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style> -->
+
+<!-- Bài 3 -->
+<!-- <script setup>
+import Bai3 from './components/Bai3.vue';
+</script>
+
+<template>
+  <Bai3/>
+</template> -->
+<!-- Bài 4 -->
+<script setup>
+import Bai4 from './components/Bai4.vue';
+</script>
+
+<template>
+  <Bai4 />
+</template>
---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+
+// Import Bootstrap 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+createApp(App).mount('#app');
